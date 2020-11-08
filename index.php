9a57c985712d4c6f07d7f760bf0d95d783914af2
Lwin Maung Maung <lwinmaungmaung90@gmail.com>
2020-11-08 11:21:03 +0630

    init commit

---
+++ index.php
@@ -0,0 +1,532 @@
+<?php
+$pyithu = Array();
+$pyithu['ayeyarwaddy']['total']=26;
+$pyithu['chin']['total']=9;
+$pyithu['kachin']['total']=18;
+$pyithu['kayar']['total']=7;
+$pyithu['kayin']['total']=7;
+$pyithu['magway']['total']=25;
+$pyithu['mandalay']['total']=28;
+$pyithu['mon']['total']=10;
+$pyithu['naypyitaw']['total']=8;
+$pyithu['pegu']['total']=28;
+$pyithu['rakhine']['total']=17;
+$pyithu['sagaing']['total']=37;
+$pyithu['shan']['total']=55;
+$pyithu['tanintharyi']['total']=10;
+$pyithu['yangon']['total']=45;
+
+$pyithu['ayeyarwaddy']['nld']=0;
+$pyithu['chin']['nld']=0;
+$pyithu['kachin']['nld']=0;
+$pyithu['kayar']['nld']=0;
+$pyithu['kayin']['nld']=0;
+$pyithu['magway']['nld']=0;
+$pyithu['mandalay']['nld']=0;
+$pyithu['mon']['nld']=0;
+$pyithu['naypyitaw']['nld']=0;
+$pyithu['pegu']['nld']=0;
+$pyithu['rakhine']['nld']=0;
+$pyithu['sagaing']['nld']=0;
+$pyithu['shan']['nld']=0;
+$pyithu['tanintharyi']['nld']=0;
+$pyithu['yangon']['nld']=0;
+
+$pyithu['ayeyarwaddy']['usdp']=0;
+$pyithu['chin']['usdp']=0;
+$pyithu['kachin']['usdp']=0;
+$pyithu['kayar']['usdp']=0;
+$pyithu['kayin']['usdp']=0;
+$pyithu['magway']['usdp']=0;
+$pyithu['mandalay']['usdp']=0;
+$pyithu['mon']['usdp']=0;
+$pyithu['naypyitaw']['usdp']=0;
+$pyithu['pegu']['usdp']=0;
+$pyithu['rakhine']['usdp']=0;
+$pyithu['sagaing']['usdp']=0;
+$pyithu['shan']['usdp']=0;
+$pyithu['tanintharyi']['usdp']=0;
+$pyithu['yangon']['usdp']=0;
+
+$pyithu['ayeyarwaddy']['other']=0;
+$pyithu['chin']['other']=0;
+$pyithu['kachin']['other']=0;
+$pyithu['kayar']['other']=0;
+$pyithu['kayin']['other']=0;
+$pyithu['magway']['other']=0;
+$pyithu['mandalay']['other']=0;
+$pyithu['mon']['other']=0;
+$pyithu['naypyitaw']['other']=0;
+$pyithu['pegu']['other']=0;
+$pyithu['rakhine']['other']=0;
+$pyithu['sagaing']['other']=0;
+$pyithu['shan']['other']=0;
+$pyithu['tanintharyi']['other']=0;
+$pyithu['yangon']['other']=0;
+
+
+
+//Amyotha
+
+$amyotha['ayeyarwaddy']['total']=12;
+$amyotha['chin']['total']=12;
+$amyotha['kachin']['total']=12;
+$amyotha['kayar']['total']=12;
+$amyotha['kayin']['total']=12;
+$amyotha['magway']['total']=12;
+$amyotha['mandalay']['total']=12;
+$amyotha['mon']['total']=12;
+$amyotha['naypyitaw']['total']=12;
+$amyotha['pegu']['total']=12;
+$amyotha['rakhine']['total']=12;
+$amyotha['sagaing']['total']=12;
+$amyotha['shan']['total']=12;
+$amyotha['tanintharyi']['total']=12;
+$amyotha['yangon']['total']=12;
+
+$amyotha['ayeyarwaddy']['nld']=0;
+$amyotha['chin']['nld']=0;
+$amyotha['kachin']['nld']=0;
+$amyotha['kayar']['nld']=0;
+$amyotha['kayin']['nld']=0;
+$amyotha['magway']['nld']=0;
+$amyotha['mandalay']['nld']=0;
+$amyotha['mon']['nld']=0;
+$amyotha['naypyitaw']['nld']=0;
+$amyotha['pegu']['nld']=0;
+$amyotha['rakhine']['nld']=0;
+$amyotha['sagaing']['nld']=0;
+$amyotha['shan']['nld']=0;
+$amyotha['tanintharyi']['nld']=0;
+$amyotha['yangon']['nld']=0;
+
+$amyotha['ayeyarwaddy']['usdp']=0;
+$amyotha['chin']['usdp']=0;
+$amyotha['kachin']['usdp']=0;
+$amyotha['kayar']['usdp']=0;
+$amyotha['kayin']['usdp']=0;
+$amyotha['magway']['usdp']=0;
+$amyotha['mandalay']['usdp']=0;
+$amyotha['mon']['usdp']=0;
+$amyotha['naypyitaw']['usdp']=0;
+$amyotha['pegu']['usdp']=0;
+$amyotha['rakhine']['usdp']=0;
+$amyotha['sagaing']['usdp']=0;
+$amyotha['shan']['usdp']=0;
+$amyotha['tanintharyi']['usdp']=0;
+$amyotha['yangon']['usdp']=0;
+
+$amyotha['ayeyarwaddy']['other']=0;
+$amyotha['chin']['other']=0;
+$amyotha['kachin']['other']=0;
+$amyotha['kayar']['other']=0;
+$amyotha['kayin']['other']=0;
+$amyotha['magway']['other']=0;
+$amyotha['mandalay']['other']=0;
+$amyotha['mon']['other']=0;
+$amyotha['naypyitaw']['other']=0;
+$amyotha['pegu']['other']=0;
+$amyotha['rakhine']['other']=0;
+$amyotha['sagaing']['other']=0;
+$amyotha['shan']['other']=0;
+$amyotha['tanintharyi']['other']=0;
+$amyotha['yangon']['other']=0;
+
+
+//tine
+
+$tine['ayeyarwaddy']['total']=90;
+$tine['chin']['total']=39;
+$tine['kachin']['total']=66;
+$tine['kayar']['total']=33;
+$tine['kayin']['total']=33;
+$tine['magway']['total']=87;
+$tine['mandalay']['total']=96;
+$tine['mon']['total']=42;
+$tine['naypyitaw']['total']=36;
+$tine['pegu']['total']=96;
+$tine['rakhine']['total']=63;
+$tine['sagaing']['total']=123;
+$tine['shan']['total']=177;
+$tine['tanintharyi']['total']=42;
+$tine['yangon']['total']=147;
+
+$tine['ayeyarwaddy']['nld']=0;
+$tine['chin']['nld']=0;
+$tine['kachin']['nld']=0;
+$tine['kayar']['nld']=0;
+$tine['kayin']['nld']=0;
+$tine['magway']['nld']=0;
+$tine['mandalay']['nld']=0;
+$tine['mon']['nld']=0;
+$tine['naypyitaw']['nld']=0;
+$tine['pegu']['nld']=0;
+$tine['rakhine']['nld']=0;
+$tine['sagaing']['nld']=0;
+$tine['shan']['nld']=0;
+$tine['tanintharyi']['nld']=0;
+$tine['yangon']['nld']=0;
+
+$tine['ayeyarwaddy']['usdp']=0;
+$tine['chin']['usdp']=0;
+$tine['kachin']['usdp']=0;
+$tine['kayar']['usdp']=0;
+$tine['kayin']['usdp']=0;
+$tine['magway']['usdp']=0;
+$tine['mandalay']['usdp']=0;
+$tine['mon']['usdp']=0;
+$tine['naypyitaw']['usdp']=0;
+$tine['pegu']['usdp']=0;
+$tine['rakhine']['usdp']=0;
+$tine['sagaing']['usdp']=0;
+$tine['shan']['usdp']=0;
+$tine['tanintharyi']['usdp']=0;
+$tine['yangon']['usdp']=0;
+
+$tine['ayeyarwaddy']['other']=0;
+$tine['chin']['other']=0;
+$tine['kachin']['other']=0;
+$tine['kayar']['other']=0;
+$tine['kayin']['other']=0;
+$tine['magway']['other']=0;
+$tine['mandalay']['other']=0;
+$tine['mon']['other']=0;
+$tine['naypyitaw']['other']=0;
+$tine['pegu']['other']=0;
+$tine['rakhine']['other']=0;
+$tine['sagaing']['other']=0;
+$tine['shan']['other']=0;
+$tine['tanintharyi']['other']=0;
+$tine['yangon']['other']=0;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Election 2020</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="h1 text-center">Myanmar Election 2020</h1>
+            <h2 class="h2 text-center">Live Results</h2>
+            <hr width="50%">
+            <h6 class="h6">Data Source: Yway Mal <span class="float-right">Powered by Lovely Family Host</span></h6>
+
+        </div>
+        <div class="col-md-12">
+            <br><br>
+            <div class="text-center electorial-pictorial">322 votes to win</div>
+            <div class="winning-icons">
+                <div class="left-side">
+                    <svg viewBox="-1 -1 38 38">
+                        <path
+                                d="M18 2.0845
+      a 15.9155 15.9155 0 0 1 0 31.831
+      a 15.9155 15.9155 0 0 1 0 -31.831"
+                                fill="none"
+                                stroke="#aa0000" ;
+                                stroke-width="3" ;
+                                stroke-dasharray="0, 100"
+                                id="left-party"
+                        />
+                    </svg>
+
+                </div>
+                <div class="left-text">0</div>
+                <div class="right-side">
+                    <svg viewBox="-1 -1 38 38">
+                        <path
+                                d="M18 2.0845
+      a 15.9155 15.9155 0 0 1 0 31.831
+      a 15.9155 15.9155 0 0 1 0 -31.831"
+                                fill="none"
+                                stroke="#00aa00" ;
+                                stroke-width="3" ;
+                                stroke-dasharray="0, 100"
+                                id="right-party"
+                        />
+                    </svg>
+                </div>
+                <div class="right-text">0</div>
+            </div>
+            <div class="party-information">
+                <div class="left-info">
+                    <h3 class="h4">NLD</h3>
+                    <h4 class="h5">0 <br>votes</h4>
+                </div>
+                <div class="right-info">
+                    <h3 class="h4">USDP</h3>
+                    <h4 class="h5">0 <br>votes</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+            <h2 class="text-center h2">Pyithu Hluttaw</h2>
+            <hr width="50%" class="text-center">
+            <div class="row">
+                <?php $i=0;?>
+                <?php foreach ($pyithu as $key=>$item):?>
+                    <?php if ($i==0 || $i == intval(count($pyithu)/2)+1): ?>
+                        <div class="col-md-6">
+                        <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">NLD</th>
+                            <th scope="col">USDP</th>
+                            <th scope="col">Other</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                    <?php endif;?>
+                    <tr>
+                        <?php
+                            if($item['nld']==0&&$item['usdp']==0&&$item['other']==0) $bgcolor="bg-secondary";
+                            else if($item['nld']>$item['usdp'] && $item['nld']>$item['other']){
+                                $bgcolor = "bg-danger";
+                            }
+                            else if($item['nld']==$item['usdp']){
+                                $bgcolor = "bg-dark";
+                            }
+
+                            else{
+                                if($item['usdp']>$item['nld'] && $item['usdp']>$item['other']){
+                                    $bgcolor = "bg-success";
+                                }
+                                else
+                                    $bgcolor = "bg-warning";
+                            }
+
+                        ?>
+
+                        <th scope="row" class="text-capitalize <?php echo $bgcolor; ?> text-white"><?php echo $key." (".$item['total'].")";?> </th>
+                        <td><?php echo number_format($item['nld']/$item['total']*100,2,'.','')."% (".$item['nld'].")" ; ?></td>
+                        <td><?php echo number_format($item['usdp']/$item['total']*100,2,'.','')."% (".$item['usdp'].")" ; ?></td>
+                        <td><?php echo number_format($item['other']/$item['total']*100,2,'.','')."% (".$item['other'].")" ; ?></td>
+                    </tr>
+                    <?php if ($i == intval(count($pyithu)/2) || $i == count($pyithu)-1):?>
+                        </tbody>
+                        </table>
+                        </div>
+                        <!--                <div class="col-md-6">-->
+                    <?php endif;?>
+                    <?php $i++; ?>
+                <?php endforeach;?>
+            </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <br>
+                <h2 class="text-center h2">Amyotha Hluttaw</h2>
+                <hr width="50%" class="text-center">
+                <div class="row">
+                    <?php $i=0;?>
+                    <?php foreach ($amyotha as $key=>$item):?>
+                        <?php if ($i==0 || $i == intval(count($pyithu)/2)+1): ?>
+                            <div class="col-md-6">
+                            <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">NLD</th>
+                                <th scope="col">USDP</th>
+                                <th scope="col">Other</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                        <?php endif;?>
+                        <tr>
+                            <?php
+                            if($item['nld']==0&&$item['usdp']==0&&$item['other']==0) $bgcolor="bg-secondary";
+                            else if($item['nld']>$item['usdp'] && $item['nld']>$item['other']){
+                                $bgcolor = "bg-danger";
+                            }
+                            else if($item['nld']==$item['usdp']){
+                                $bgcolor = "bg-dark";
+                            }
+
+                            else{
+                                if($item['usdp']>$item['nld'] && $item['usdp']>$item['other']){
+                                    $bgcolor = "bg-success";
+                                }
+                                else
+                                    $bgcolor = "bg-warning";
+                            }
+
+                            ?>
+
+                            <th scope="row" class="text-capitalize <?php echo $bgcolor; ?> text-white"><?php echo $key." (".$item['total'].")";?> </th>
+                            <td><?php echo number_format($item['nld']/$item['total']*100,2,'.','')."% (".$item['nld'].")" ; ?></td>
+                            <td><?php echo number_format($item['usdp']/$item['total']*100,2,'.','')."% (".$item['usdp'].")" ; ?></td>
+                            <td><?php echo number_format($item['other']/$item['total']*100,2,'.','')."% (".$item['other'].")" ; ?></td>
+                        </tr>
+                        <?php if ($i == intval(count($pyithu)/2) || $i == count($pyithu)-1):?>
+                            </tbody>
+                            </table>
+                            </div>
+                            <!--                <div class="col-md-6">-->
+                        <?php endif;?>
+                        <?php $i++; ?>
+                    <?php endforeach;?>
+                </div>
+
+            </div>
+        </div>
+    <div class="row">
+        <div class="col-md-12">
+            <br>
+            <h2 class="text-center h2">State/Division Hluttaw</h2>
+            <hr width="50%" class="text-center">
+            <div class="row">
+                <?php $i=0;?>
+                <?php foreach ($tine as $key=>$item):?>
+                    <?php if ($i==0 || $i == intval(count($pyithu)/2)+1): ?>
+                        <div class="col-md-6">
+                        <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">NLD</th>
+                            <th scope="col">USDP</th>
+                            <th scope="col">Other</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                    <?php endif;?>
+                    <tr>
+                        <?php
+                        if($item['nld']==0&&$item['usdp']==0&&$item['other']==0) $bgcolor="bg-secondary";
+                        else if($item['nld']>$item['usdp'] && $item['nld']>$item['other']){
+                            $bgcolor = "bg-danger";
+                        }
+                        else if($item['nld']==$item['usdp']){
+                            $bgcolor = "bg-dark";
+                        }
+
+                        else{
+                            if($item['usdp']>$item['nld'] && $item['usdp']>$item['other']){
+                                $bgcolor = "bg-success";
+                            }
+                            else
+                                $bgcolor = "bg-warning";
+                        }
+
+                        ?>
+
+                        <th scope="row" class="text-capitalize <?php echo $bgcolor; ?> text-white"><?php echo $key." (".$item['total'].")";?> </th>
+                        <td><?php echo number_format($item['nld']/$item['total']*100,2,'.','')."% (".$item['nld'].")" ; ?></td>
+                        <td><?php echo number_format($item['usdp']/$item['total']*100,2,'.','')."% (".$item['usdp'].")" ; ?></td>
+                        <td><?php echo number_format($item['other']/$item['total']*100,2,'.','')."% (".$item['other'].")" ; ?></td>
+                    </tr>
+                    <?php if ($i == intval(count($pyithu)/2) || $i == count($pyithu)-1):?>
+                        </tbody>
+                        </table>
+                        </div>
+                        <!--                <div class="col-md-6">-->
+                    <?php endif;?>
+                    <?php $i++; ?>
+                <?php endforeach;?>
+            </div>
+
+        </div>
+    </div>
+</div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <svg width="100%" height="100%" viewBox="-30 -30 150 150" style="max-width:100%; transform: rotate(180deg)">
+                <?php
+                for ($i = 1; $i <= 300; $i++) {
+                    ?>
+                    <circle id="constituencies<?php echo $i ?>" cx="3.339" cy="3.339" r="1.339" fill="none" stroke-width="0.1" stroke="#aaaaaa"/>
+                    <?php
+                }
+                ?>
+            </svg>
+        </div>
+    </div>
+</div>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    $("#left-party").attr('stroke-dasharray', '0,100');
+    $("#right-party").attr('stroke-dasharray', '0,100');
+    var constituencies = 0;
+    for (i = 1; i <= 17; i++) {
+        for (j = 1; j < (i + 10); j++) {
+            constituencies++;
+            var item = constituencies;
+            // console.log(j);
+            // console.log(constituencies);
+            $("#constituencies" + item).attr('cx', (50 + ((i * 3 + 10)) * Math.cos(j * (180/(i+10)) * Math.PI / 180)));
+            $("#constituencies" + item).attr('cy', (50 + ((i * 3 + 10)) * Math.sin(j * (180/(i+10)) * Math.PI / 180)));
+        }
+    }
+    var pointer= 1;
+    var item_per_row = 10;
+    var row=1;
+    var column=1;
+    for(a=1;a<=180;a++){
+        // console.log(a,pointer,row,column,item_per_row);
+        $("#constituencies" + (pointer)).attr("fill", '#FF0000');
+        pointer = column+fabonanci(item_per_row++);
+        row++;
+        if(a%17==0){
+            row=1;
+            column++;
+            pointer=column;
+            item_per_row=10;
+        }
+    }
+    for(a=181;a<=230;a++){
+        // console.log(a,pointer,row,column,item_per_row);
+        $("#constituencies" + (pointer)).attr("fill", '#00AA00');
+        pointer = column+fabonanci(item_per_row++);
+        row++;
+        if(a%17==0){
+            row=1;
+            column++;
+            pointer=column;
+            item_per_row=10;
+        }
+    }
+    // var pointer= 1;
+    // var item_per_row = 10;
+    // var row=1;
+    // var column=1;
+    // for(a=1;a<=20;a++){
+    //     pointer = (pointer==1)? pointer+item_per_row-1: column+fabonanci(item_per_row++)+item_per_row-1;
+    //     console.log(a,pointer,row,column,item_per_row);
+    //     $("#constituencies" + (pointer)).attr("fill", '#00AA00');
+    //     row++;
+    //     if(a%16==0){
+    //         row=1;
+    //         column++;
+    //         pointer=row-1;
+    //         item_per_row=10;
+    //     }
+    // }
+    function fabonanci(num){
+        return (num<=10)? num: num + fabonanci(num - 1);
+    }
+
+</script>
+</html>

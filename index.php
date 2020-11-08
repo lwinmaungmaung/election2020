413d8ef01f10d47b7112771b15cefdd07c7a5639
Lwin Maung Maung <lwinmaungmaung90@gmail.com>
2020-11-08 18:23:48 +0630

    longlone unconfirmed

--- index.php
+++ index.php
@@ -199,8 +199,10 @@ $tine['shan']['other']=0;
 $tine['tanintharyi']['other']=0;
 $tine['yangon']['other']=0;
 
-$totalvotes['left'] = 79+99+100+80+6+3+1154+448+1395+1397;
-$totalvotes['right'] = 29+8+4+29+237+251+602+0+896+419;
+$left_unconfirmed = 430;
+$totalvotes['left'] = $left_unconfirmed+79+99+100+80+6+3+1154+448+1395+1397;
+$right_unconfirmed = 173;
+$totalvotes['right'] = $right_unconfirmed+29+8+4+29+237+251+602+0+896+419;
 
 ?>
 
